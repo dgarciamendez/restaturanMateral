f0cb5bb2e50eba069f4839c1bcad75bc1b97f93b
dgarciamendez <dgarciamendez@gmail.com>
2025-11-23 07:13:40 -0700

    create proyect

---
+++ src/app/Feature/caja/caja.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-caja',
+  standalone: true,
+  imports: [],
+  templateUrl: './caja.component.html',
+  styleUrl: './caja.component.scss'
+})
+export class CajaComponent {
+
+}
---
+++ src/app/Feature/domicilio/domicilio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-domicilio',
+  standalone: true,
+  imports: [],
+  templateUrl: './domicilio.component.html',
+  styleUrl: './domicilio.component.scss'
+})
+export class DomicilioComponent {
+
+}
